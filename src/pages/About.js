e59d769a32af9dcfe82146fc03f81c7d0956b6f3
路明 <snaillu@yeah.net>
2020-07-02 15:19:05 +0800

    增加路由功能

---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const About = () => {
+    return (
+        <div>这里是About页</div>
+    )
+}
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Weather from '../containers/Weather';
+
+const Home = () => {
+    return (
+        <div>
+            <h4>这里是Home页</h4>
+            <Weather />
+        </div>
+    )
+}
+
+export default Home;
