9de9effe19e79cb78ee1f6ed700b14c696515e45
Snail-Lu <snaillu@yeah.net>
2020-12-23 23:47:36 +0800

    首页界面调整

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,33 +8,22 @@ const Home = () => {
             {/* <p>Hey, </p>
             <p>Here is My Portfolio~</p>
             <p>ψ(｀∇´)ψ</p> */}
-            {/* <div className="title">
-                Portfolio
+            <div className={styles.title}>
+                <p className={styles.title_txt}>Portfolio</p>
             </div>
-            <div className="tab-list">
+            <div className={styles.tab_list}>
                 <NavLink to="/" exact activeClassName={styles.nav_item_selected}>
-                    <div className={styles.nav_item}>Home</div>
+                    <div className={styles.tab_item}>Home</div>
                 </NavLink>
                 <NavLink to="/wroks" activeClassName={styles.nav_item_selected}>
-                    <div className={styles.nav_item}>Works</div>
+                    <div className={styles.tab_item}>Works</div>
                 </NavLink>
                 <NavLink to="/whims" activeClassName={styles.nav_item_selected}>
-                    <div className={styles.nav_item}>Whims</div>
+                    <div className={styles.tab_item}>Whims</div>
                 </NavLink>
                 <NavLink to="/functional" activeClassName={styles.nav_item_selected}>
-                    <div className={styles.nav_item}>About</div>
+                    <div className={styles.tab_item}>About</div>
                 </NavLink>
-            </div> */}
-
-            <div id={styles.clouds}>
-                <div className={styles.cloud1}></div>
-                <div className={styles.cloud2}></div>
-                <div className={styles.cloud3}></div>
-            </div>
-            <div id={styles.clouds2}>
-                <div className={styles.cloud1}></div>
-                <div className={styles.cloud2}></div>
-                <div className={styles.cloud3}></div>
             </div>
         </div>
     )
