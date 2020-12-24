9334aec9c4b30e9f349b5c2030038a3e25065613
路明 <snaillu@yeah.net>
2020-12-24 16:38:17 +0800

    首页布局调整

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,25 +5,15 @@ import { NavLink } from 'react-router-dom';
 const Home = () => {
     return (
         <div className={styles.home}>
-            {/* <p>Hey, </p>
-            <p>Here is My Portfolio~</p>
-            <p>ψ(｀∇´)ψ</p> */}
             <div className={styles.title}>
                 <p className={styles.title_txt}>Portfolio</p>
             </div>
             <div className={styles.tab_list}>
-                <NavLink to="/" exact activeClassName={styles.nav_item_selected}>
-                    <div className={styles.tab_item}>Home</div>
-                </NavLink>
-                <NavLink to="/wroks" activeClassName={styles.nav_item_selected}>
-                    <div className={styles.tab_item}>Works</div>
-                </NavLink>
-                <NavLink to="/whims" activeClassName={styles.nav_item_selected}>
-                    <div className={styles.tab_item}>Whims</div>
-                </NavLink>
-                <NavLink to="/functional" activeClassName={styles.nav_item_selected}>
-                    <div className={styles.tab_item}>About</div>
-                </NavLink>
+                <NavLink to="/" exact className={styles.tab_item}><span>Home</span></NavLink>
+                <NavLink to="/wroks" className={styles.tab_item}><span>Works</span></NavLink>
+                <NavLink to="/whims" className={styles.tab_item}><span>Whims</span></NavLink>
+                <NavLink to="/about" className={styles.tab_item}><span>About</span></NavLink>
+                <NavLink to="/about" className={styles.tab_item}><span>About</span></NavLink>
             </div>
         </div>
     )
