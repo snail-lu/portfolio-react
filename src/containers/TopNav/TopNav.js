d96cfc2a9085207907a6ad718711e779065feffc
路明 <snaillu@yeah.net>
2020-12-25 18:06:37 +0800

    TopNav组件优化

--- src/containers/TopNav/TopNav.js
+++ src/containers/TopNav/TopNav.js
@@ -4,26 +4,10 @@ import { NavLink } from 'react-router-dom';
 import styles from './TopNav.module.scss';
 import { showNav } from '../../redux/actions';
 import { SHOW_NAV, HIDE_NAV } from '../../redux/actionTypes';
-import avatarUrl from '../../assets/images/avatar.jpg';
 
 const TopNav = ({show,switchClick,routerid}) => {
-    // if(show){
         return (
-            // <div className={`${styles.left_nav} ${styles.left_nav_to_show}`}>
-            //     <div className={styles.left_nav_content}>
-            //             <NavLink to="/" exact activeClassName={styles.nav_item_selected}>
-            //                 <div className={styles.nav_item}>首页</div>
-            //             </NavLink>
-            //             <NavLink to="/effect" activeClassName={styles.nav_item_selected}>
-            //                 <div className={styles.nav_item}>特效型</div>
-            //             </NavLink>
-            //             <NavLink to="/functional" activeClassName={styles.nav_item_selected}>
-            //                 <div className={styles.nav_item}>功能型</div>
-            //             </NavLink>
-            //     </div>
-            //      <div className={styles.hide_btn} onClick={()=>switchClick(HIDE_NAV)}>&#171;</div>
-            // </div>
-            <div className={styles.top_nav}>
+            <div className={styles.top_nav_container}>
               <div className={`${styles.menu_bar} ${show?styles.active:""}`} onClick={()=>switchClick(show?HIDE_NAV:SHOW_NAV)}>
                 {
                   show
