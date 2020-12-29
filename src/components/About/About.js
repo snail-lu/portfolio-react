ec3cf41b53b30a301bd03e49f64027902d225709
Snail-Lu <snaillu@yeah.net>
2020-12-29 22:32:42 +0800

    about页面增加内容

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import styles from './About.module.scss';
+import { NavLink } from 'react-router-dom';
 
 const About = () => {
     return (
-        <div className={styles.about_me_container}>About</div>
+        <div className={styles.about_me_container}>
+            <div className={styles.about_item}><a href="https://github.com/snail-lu">GitHub</a></div>
+            <div className={styles.about_item}><a href="https://yes-i-do.cn">个人网站</a></div>
+            <div className={styles.about_item}><a href="https://www.cnblogs.com/snaillu/">Blogs</a></div>
+            <div className={styles.about_item}><a href="mailto:snaillu@yeah.net">Email</a></div>
+        </div>
     )
 }
 
