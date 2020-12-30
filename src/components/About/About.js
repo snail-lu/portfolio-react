8d78c8186965e30ab48d12550cc42bb0ef0000fb
Snail-Lu <snaillu@yeah.net>
2020-12-30 21:55:04 +0800

    About页样式调整

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import styles from './About.module.scss';
-import { NavLink } from 'react-router-dom';
 
 const About = () => {
     return (
         <div className={styles.about_me_container}>
-            <div className={styles.about_item}><a href="https://github.com/snail-lu">GitHub</a></div>
-            <div className={styles.about_item}><a href="https://yes-i-do.cn">个人网站</a></div>
-            <div className={styles.about_item}><a href="https://www.cnblogs.com/snaillu/">Blogs</a></div>
-            <div className={styles.about_item}><a href="mailto:snaillu@yeah.net">Email</a></div>
+            <div className={styles.about_item}><a href="https://github.com/snail-lu" target="_blank">GitHub</a></div>
+            <div className={styles.about_item}><a href="https://yes-i-do.cn">Site</a></div>
+            <div className={styles.about_item}><a href="https://www.cnblogs.com/snaillu/" target="_blank">Blogs</a></div>
+            <div className={styles.about_item}><a href="mailto:snaillu@yeah.net" target="_blank">Email</a></div>
         </div>
     )
 }
