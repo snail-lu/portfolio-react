1b1601ba70b0b766e66b60f8aeb4ca113128a109
Snail-Lu <snaillu@yeah.net>
2021-03-24 23:13:36 +0800

    歌曲歌词改为对象数组

--- src/components/AudioPlayer/AudioPlayer.js
+++ src/components/AudioPlayer/AudioPlayer.js
@@ -12,111 +12,220 @@ export default class AudioPlayer extends Component {
             playedTime: "00:00",      // 已播放时间 
             duration: 0,              // 音乐总时长（未转换）
             totalTime: "00:00",
-            lyric: "有何不可\n" +
-            "\n" +
-            "作词：许嵩 作曲：许嵩\n" +
-            "\n" +
-            "演唱：许嵩\n" +
-            "\n" +
-            "天空好想下雨\n" +
-            "\n" +
-            "我好想住你隔壁\n" +
-            "\n" +
-            "傻站在你家楼下\n" +
-            "\n" +
-            "抬起头 数乌云\n" +
-            "\n" +
-            "如果场景里出现一架钢琴\n" +
-            "\n" +
-            "我会唱歌给你听\n" +
-            "\n" +
-            "哪怕好多盆水往下淋\n" +
-            "\n" +
-            "夏天快要过去\n" +
-            "\n" +
-            "请你少买冰淇淋\n" +
-            "\n" +
-            "天凉就别穿短裙\n" +
-            "\n" +
-            "别再那么淘气\n" +
-            "\n" +
-            "如果有时不那么开心\n" +
-            "\n" +
-            "我愿意将格洛米借给你\n" +
-            "\n" +
-            "你其实明白我心意\n" +
-            "\n" +
-            "为你唱这首歌 没有什么风格\n" +
-            "\n" +
-            "它仅仅代表着 我想给你快乐\n" +
-            "\n" +
-            "为你解冻冰河 为你做一只扑火的飞蛾\n" +
-            "\n" +
-            "没有什么事情是不值得\n" +
-            "\n" +
-            "为你唱这首歌 没有什么风格\n" +
-            "\n" +
-            "它仅仅代表着 我希望你快乐\n" +
-            "\n" +
-            "为你辗转反侧 为你放弃世界有何不可\n" +
-            "\n" +
-            "夏末秋凉里带一点温热 有换季的颜色\n" +
-            "\n" +
-            "天空好想下雨\n" +
-            "\n" +
-            "我好想住你隔壁\n" +
-            "\n" +
-            "傻站在你家楼下\n" +
-            "\n" +
-            "抬起头 数乌云\n" +
-            "\n" +
-            "如果场景里出现一架钢琴\n" +
-            "\n" +
-            "我会唱歌给你听\n" +
-            "\n" +
-            "哪怕好多盆水往下淋\n" +
-            "\n" +
-            "夏天快要过去\n" +
-            "\n" +
-            "请你少买冰淇淋\n" +
-            "\n" +
-            "天凉就别穿短裙\n" +
-            "\n" +
-            "别再那么淘气\n" +
-            "\n" +
-            "如果有时不那么开心\n" +
-            "\n" +
-            "我愿意将格洛米借给你\n" +
-            "\n" +
-            "你其实明白我心意\n" +
-            "\n" +
-            "为你唱这首歌 没有什么风格\n" +
-            "\n" +
-            "它仅仅代表着 我想给你快乐\n" +
-            "\n" +
-            "为你解冻冰河 为你做一只扑火的飞蛾\n" +
-            "\n" +
-            "没有什么事情是不值得\n" +
-            "\n" +
-            "为你唱这首歌 没有什么风格\n" +
-            "\n" +
-            "它仅仅代表着 我希望你快乐\n" +
-            "\n" +
-            "为你辗转反侧 为你放弃世界有何不可\n" +
-            "\n" +
-            "夏末秋凉里带一点温热\n" +
-            "\n" +
-            "为你解冻冰河 为你做一只扑火的飞蛾\n" +
-            "\n" +
-            "没有什么事情是不值得\n" +
-            "\n" +
-            "为你唱这首歌 没有什么风格\n" +
-            "\n" +
-            "它仅仅代表着 我希望你快乐\n" +
-            "\n" +
-            "为你辗转反侧 为你放弃世界有何不可\n" +
-            "\n" +
-            "夏末秋凉里带一点温热 有换季的颜色"
+            lyricArray: [
+                {
+                  t: 0,
+                  c: '有何不可'
+                },
+                {
+                    t: 1,
+                    c: "作词：许嵩 作曲：许嵩"
+                },
+                {
+                    t: 2,
+                    c: '演唱：许嵩'
+                },
+                {
+                    t: 21,
+                    c: '天空好想下雨'
+                },
+                {
+                    t: 25,
+                    c: '我好想住你隔壁'
+                },
+                {
+                    t: 27,
+                    c: '傻站在你家楼下'
+                },
+                {
+                    t: 29,
+                    c: '抬起头 数乌云'
+                },
+                {
+                    t: 31,
+                    c: '如果场景里出现一架钢琴'
+                }, 
+                {
+                    t: 33,
+                    c: '我会唱歌给你听'
+                }, 
+                {
+                    t: 36,
+                    c: '哪怕好多盆水往下淋'
+                }, 
+                {
+                    t: 42,
+                    c: '夏天快要过去'
+                }, 
+                {
+                    t: 44,
+                    c: '请你少买冰淇淋'
+                }, 
+                { 
+                    t: 46,
+                    c: '天凉就别穿短裙'
+                },
+                { 
+                    t: 48,
+                    c: '别再那么淘气'
+                },
+                { 
+                    t: 51,
+                    c: '如果有时不那么开心'
+                },
+                { 
+                    t: 52,
+                    c: '我愿意将格洛米借给你'
+                },
+                { 
+                    t: 54,
+                    c: '你其实明白我心意'
+                },
+                { 
+                    t: 59,
+                    c: '为你唱这首歌 没有什么风格'
+                },
+                { 
+                    t: 63,
+                    c: '它仅仅代表着 我想给你快乐'
+                },
+                {
+                    t: 68,
+                    c: '为你解冻冰河 为你做一只扑火的飞蛾'
+                },
+                {
+                    t: 74,
+                    c: '没有什么事情是不值得'
+                },
+                {
+                    t: 78,
+                    c: '为你唱这首歌 没有什么风格'
+                },
+                {
+                    t: 82,
+                    c: '它仅仅代表着 我希望你快乐'
+                },
+                {
+                    t: 87,
+                    c: '为你辗转反侧 为你放弃世界有何不可'
+                },
+                {
+                    t: 93,
+                    c: '夏末秋凉里带一点温热 有换季的颜色'
+                },
+                {
+                    t: 117,
+                    c: '天空好想下雨'
+                },
+                {
+                    t: 119,
+                    c: '我好想住你隔壁'
+                },
+                {
+                    t: 122,
+                    c: '傻站在你家楼下'
+                },
+                {
+                    t: 125,
+                    c: '抬起头 数乌云'
+                },
+                {
+                    t: 127,
+                    c: '如果场景里出现一架钢琴'
+                },
+                {
+                    t: 128,
+                    c: '我会唱歌给你听'
+                },
+                {
+                    t: 132,
+                    c: '哪怕好多盆水往下淋'
+                },
+                {
+                    t: 135,
+                    c: '夏天快要过去'
+                },
+                {
+                    t: 137,
+                    c: '请你少买冰淇淋'
+                },
+                {
+                    t: 0,
+                    c: '天凉就别穿短裙'
+                },
+                {
+                    t: 141,
+                    c: '别再那么淘气'
+                },
+                {
+                    t: 145,
+                    c: '如果有时不那么开心'
+                },
+                {
+                    t: 147,
+                    c: '我愿意将格洛米借给你'
+                },
+                {
+                    t: 150,
+                    c: '你其实明白我心意'
+                },
+                {
+                    t: 154,
+                    c: '为你唱这首歌 没有什么风格'
+                },
+                {
+                    t: 157,
+                    c: '它仅仅代表着 我想给你快乐'
+                },
+                {
+                    t: 163,
+                    c: '为你解冻冰河 为你做一只扑火的飞蛾'
+                },
+                {
+                    t: 168,
+                    c: '没有什么事情是不值得'
+                },
+                {
+                    t: 171,
+                    c: '为你唱这首歌 没有什么风格'
+                },
+                {
+                    t: 177,
+                    c: '它仅仅代表着 我希望你快乐'
+                },
+                {
+                    t: 182,
+                    c: '为你辗转反侧 为你放弃世界有何不可'
+                },
+                {
+                    t: 188,
+                    c: '夏末秋凉里带一点温热'
+                },
+                {
+                    t: 200,
+                    c: '为你解冻冰河 为你做一只扑火的飞蛾'
+                },
+                {
+                    t: 207,
+                    c: '没有什么事情是不值得'
+                },
+                {
+                    t: 210,
+                    c: '为你唱这首歌 没有什么风格'
+                },
+                {
+                    t: 215,
+                    c: '它仅仅代表着 我希望你快乐'
+                },
+                {
+                    t: 220,
+                    c: '为你辗转反侧 为你放弃世界有何不可'
+                },
+                {
+                    t: 225,
+                    c: '夏末秋凉里带一点温热 有换季的颜色'
+                }
+            ],
         }
     }
 
@@ -167,8 +276,9 @@ export default class AudioPlayer extends Component {
         currentTime_s = currentTime_s>=10?currentTime_s:"0"+ currentTime_s;
 
         const lyricHeight = this.lyricDom.clientHeight;
+        console.log(lyricHeight)
         this.setState({
-            lyricPosition: -Math.floor(lyricHeight*currentTime/this.audio.duration)+'px',
+            lyricPosition: -Math.floor(lyricHeight*currentTime)+'px',
             currentTime,
             playedTime: currentTime_m + ":" + currentTime_s
         })
@@ -180,7 +290,7 @@ export default class AudioPlayer extends Component {
     }
 
     render() {
-        let { paused, playedTime, totalTime, currentTime, duration, lyricPosition} = this.state;
+        let { paused, playedTime, totalTime, currentTime, duration, lyricPosition, lyricArray } = this.state;
         return (
                 <div className={styles.music_container}>
                     <div className={styles.header}>音乐播放器</div>
@@ -192,11 +302,18 @@ export default class AudioPlayer extends Component {
                                 </div>
                             </div>
                         </div>
-                        <div className={styles.lyric}>
-                            <pre
-                                className={styles.lyric_text}
-                                style={{top: lyricPosition}}
-                                ref={(lyricDom) => { this.lyricDom = lyricDom  }}>{this.state.lyric}</pre>
+                        <div className={styles.lyric} style={{marginTop: lyricPosition}}>
+                            {
+                                lyricArray.map(lyric=>{
+                                    return (
+                                        <p 
+                                          className={`${styles.lyric_text} ${currentTime.toFixed()==lyric.t?styles.lyric_text_current:''}`}
+                                          ref={(lyricDom) => { this.lyricDom = lyricDom  }}
+                                        >{lyric.c}</p>
+                                    )
+
+                                })
+                            }
                         </div>
                     </div>
                     <div className={styles.footer}>
