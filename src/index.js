217715575fe40a1a848ff005c6785ab56efecdc6
Snail-Lu <snaillu@yeah.net>
2021-12-24 15:45:41 +0800

    配置修改

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import './assets/iconfont/iconfont.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import './assets/iconfont/iconfont.css'
 import { Provider } from 'react-redux'
-import store from './redux/store';
-import { BrowserRouter as Router } from 'react-router-dom';
+import store from './redux/store'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
 import routeConfig from './router/index'
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router basename="/portfolio">{ renderRoutes(routeConfig) }</Router>
+    <Router basename="/portfolio-react">{renderRoutes(routeConfig)}</Router>
   </Provider>,
   document.getElementById('root')
-);
+)
