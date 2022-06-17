1f7bcc9a119573cd58bd324101261ed47c69b7e7
Snail-Lu <snaillu@yeah.net>
2022-06-17 10:26:10 +0800

    useNavigate替换withRouter

--- src/components/DemoLayout/DemoLayout.js
+++ src/components/DemoLayout/DemoLayout.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import styles from './DemoLayout.module.scss';
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import styles from './DemoLayout.module.scss'
 
 const DemoLayout = (props) => {
-  const { children, title } = props;
-  
+  const { children, title } = props
+  let navigate = useNavigate()
+
   return (
     <div className={styles.demoDetailContainer}>
-        <div className={styles.title}>
-            <span onClick={()=>props.history.goBack()} className={styles.back_icon}></span>
-            <span>{ title }</span>
-        </div>
-        { children }
+      <div className={styles.title}>
+        <span onClick={() => navigate(-1)} className={styles.back_icon}></span>
+        <span>{title}</span>
+      </div>
+      {children}
     </div>
   )
 }
 
-export default DemoLayout;
+export default DemoLayout
