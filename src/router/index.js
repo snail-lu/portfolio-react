47477f39efa1e44987002d691730ce0dd5b5632a
Snail-Lu <snaillu@yeah.net>
2022-06-17 10:18:51 +0800

    组件懒加载bug修复

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
-const Home = React.lazy(() => import('../pages/Home/Home'))
+import Home from '../pages/Home/Home'
+import Layout from '../components/Layout/Layout'
 const Projects = React.lazy(() => import('../pages/Projects/Projects'))
 const DemoList = React.lazy(() => import('../pages/DemoList/DemoList'))
 const About = React.lazy(() => import('../pages/About/About'))
-const Layout = React.lazy(() => import('../components/Layout/Layout'))
 const AudioPlayer = React.lazy(() => import('../components/AudioPlayer/AudioPlayer'))
 const DigitalClock = React.lazy(() => import('../components/DigitalClock/DigitalClock'))
 const PointerClock = React.lazy(() => import('../components/PointerClock/PointerClock'))
@@ -11,69 +11,81 @@ const GreedySnake = React.lazy(() => import('../components/GreedySnake/GreedySna
 const DemoLayout = React.lazy(() => import('../components/DemoLayout/DemoLayout'))
 
 const routeConfig = [
-    {
-        path: '/',
-        element: <Layout />,
-        children: [
-            {
-                index: true,
-                element: <Home />
-            },
-            {
-                path: '/projects',
-                element: <Projects />
-            },
-            {
-                path: '/about',
-                element: <About />
-            },
-            {
-                path: '/demo',
-                // exact: true,
-                element: <DemoList />
-            },
-            {
-                path: '/demo/audioplayer',
-                render: () => {
-                    return (
-                        <DemoLayout title="音乐播放器">
-                            <AudioPlayer />
-                        </DemoLayout>
-                    )
-                }
-            },
-            {
-                path: '/demo/digitalclock',
-                render: () => {
-                    return (
-                        <DemoLayout title="数字时钟">
-                            <DigitalClock />
-                        </DemoLayout>
-                    )
-                }
-            },
-            {
-                path: '/demo/pointerclock',
-                render: () => {
-                    return (
-                        <DemoLayout title="指针时钟">
-                            <PointerClock />
-                        </DemoLayout>
-                    )
-                }
-            },
-            {
-                path: '/demo/greedysnake',
-                render: () => {
-                    return (
-                        <DemoLayout title="贪吃蛇">
-                            <GreedySnake />
-                        </DemoLayout>
-                    )
-                }
-            }
-        ]
-    }
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: '/projects',
+        element: (
+          <React.Suspense fallback={<>...</>}>
+            <Projects />
+          </React.Suspense>
+        )
+      },
+      {
+        path: '/about',
+        element: (
+          <React.Suspense fallback={<>...</>}>
+            <About />
+          </React.Suspense>
+        )
+      },
+      {
+        path: '/demo',
+        // exact: true,
+        element: (
+          <React.Suspense fallback={<>...</>}>
+            <DemoList />
+          </React.Suspense>
+        )
+      },
+      {
+        path: '/demo/audioplayer',
+        element: (
+          <React.Suspense fallback={<>...</>}>
+            <DemoLayout title="音乐播放器">
+              <AudioPlayer />
+            </DemoLayout>
+          </React.Suspense>
+        )
+      },
+      {
+        path: '/demo/digitalclock',
+        element: (
+          <React.Suspense fallback={<>...</>}>
+            <DemoLayout title="数字时钟">
+              <DigitalClock />
+            </DemoLayout>
+          </React.Suspense>
+        )
+      },
+      {
+        path: '/demo/pointerclock',
+        element: (
+          <React.Suspense fallback={<>...</>}>
+            <DemoLayout title="指针时钟">
+              <PointerClock />
+            </DemoLayout>
+          </React.Suspense>
+        )
+      },
+      {
+        path: '/demo/greedysnake',
+        element: (
+          <React.Suspense fallback={<>...</>}>
+            <DemoLayout title="贪吃蛇">
+              <GreedySnake />
+            </DemoLayout>
+          </React.Suspense>
+        )
+      }
+    ]
+  }
 ]
 
 export default routeConfig
