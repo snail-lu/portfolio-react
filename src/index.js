ac1af8991e7c69992dff6768837f219617058c28
Snail-Lu <snaillu@yeah.net>
2022-06-18 14:30:58 +0800

    iconfont字体图标引入

--- src/index.js
+++ src/index.js
@@ -3,19 +3,20 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import './assets/iconfont/iconfont.css'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
