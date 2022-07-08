991135f01f9336dd61e6f922f565ab1128da0652
Snail-Lu <snaillu@yeah.net>
2022-07-08 16:52:14 +0800

    topNav调整

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -1,53 +1,28 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import styles from './TopNav.module.scss'
-import { showNav } from '../../redux/actions'
-import { SHOW_NAV, HIDE_NAV } from '../../redux/actionTypes'
+import logoUrl from '../../assets/icons/logo.png'
 
-const TopNav = ({ show, switchClick }) => {
-  return (
-    <div className={styles.topNavContainer}>
-      <div
-        className={`${styles.menu_bar} ${show ? styles.active : ''}`}
-        onClick={() => switchClick(show ? HIDE_NAV : SHOW_NAV)}
-      >
-        {show ? (
-          <div className={styles.menu_list}>
-            <NavLink to="/" className={styles.menu_item} onClick={(e) => e.stopPropagation()}>
-              HOME
-            </NavLink>
-            <NavLink to="/projects" className={styles.menu_item} onClick={(e) => e.stopPropagation()}>
-              PROJECT
-            </NavLink>
-            <NavLink to="/demo" className={styles.menu_item} onClick={(e) => e.stopPropagation()}>
-              DEMO
-            </NavLink>
-            <NavLink to="/about" className={styles.menu_item} onClick={(e) => e.stopPropagation()}>
-              ABOUT
-            </NavLink>
-          </div>
-        ) : (
-          'MENU'
-        )}
-      </div>
-      <div></div>
-    </div>
-  )
+const TopNav = () => {
+    return (
+        <div className={styles.topNavContainer}>
+            <div className="flex-box flex-box-v-center">
+                <img src={logoUrl} className={styles.logo} />
+                <div className={`${styles.menu_list} flex-box`}>
+                    <NavLink to="/" className={styles.menu_item} onClick={(e) => e.stopPropagation()}>
+                        PROJECT
+                    </NavLink>
+                    <NavLink to="/demo" className={styles.menu_item} onClick={(e) => e.stopPropagation()}>
+                        DEMO
+                    </NavLink>
+                </div>
+                <a href="https://github.com/Snail-Lu/portfolio-react" className={styles.icon_github} target="_blank">
+                    <span className="iconfont icon-github" style={{ 'font-size': '2.5rem' }}></span>
+                </a>
+            </div>
+            <div></div>
+        </div>
+    )
 }
 
-const mapStateToProps = (state) => {
-  return {
-    show: state.showNav
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    switchClick: (type) => {
-      dispatch(showNav(type))
-    }
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(TopNav)
+export default TopNav
