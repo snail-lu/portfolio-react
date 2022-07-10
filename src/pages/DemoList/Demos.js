7c126fce22dea54fbccfc36b86a58be03ace95c6
Snail-Lu <snaillu@yeah.net>
2022-07-10 17:11:05 +0800

    update demolist

---
+++ src/pages/DemoList/Demos.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+import styles from './Demos.module.scss'
+import { NavLink } from 'react-router-dom'
+// import projectList from '../../config/project-list'
+
+export default class Demos extends Component {
+    state = {
+        demoList: []
+    }
+    componentDidMount() {
+        this.getDemoList()
+    }
+
+    getDemoList = async () => {
+        let res = await this.req({
+            url: '/demo/list2'
+        })
+        if (res.data && res.data.list) {
+            this.setState({
+                demoList: res.data.list
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div className={styles.projectsContainer}>
+                {this.state.demoList.map((demo) => {
+                    return (
+                        <div className={`${styles.project_item} flex-box`} key={demo.path}>
+                            <div className={styles.box_l}>
+                                <img className={styles.cover} src={demo.coverUrl} alt="" />
+                            </div>
+                            <div className={`${styles.box_r} flex-item-1 flex-box-column flex-box-h-between`}>
+                                <div>
+                                    <div className={styles.name}>{demo.title}</div>
+                                    <div className={styles.tags}>
+                                        {demo.tags.map((tag) => (
+                                            <span className={styles.tag} key={tag}>
+                                                {tag}
+                                            </span>
+                                        ))}
+                                    </div>
+                                    <div className={styles.desc}>{demo.desc}</div>
+                                </div>
+
+                                <div className="flex-box flex-box-h-between flex-box-v-center">
+                                    <div>
+                                        {demo.path && (
+                                            <NavLink to={`/demo/${demo.path}`}>
+                                                <span
+                                                    className="iconfont icon-entrance"
+                                                    style={{ fontSize: '2rem' }}
+                                                ></span>
+                                            </NavLink>
+                                        )}
+                                    </div>
+                                    <div className={styles.date}>{demo.date}</div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Layout from '../components/Layout/Layout'
 const Projects = React.lazy(() => import('../pages/Projects/Projects'))
-const DemoList = React.lazy(() => import('../pages/DemoList/DemoList'))
+// const DemoList = React.lazy(() => import('../pages/DemoList/DemoList'))
+const DemoList = React.lazy(() => import('../pages/DemoList/Demos'))
 const AudioPlayer = React.lazy(() => import('../components/AudioPlayer/AudioPlayer'))
 const DigitalClock = React.lazy(() => import('../components/DigitalClock/DigitalClock'))
 const PointerClock = React.lazy(() => import('../components/PointerClock/PointerClock'))
