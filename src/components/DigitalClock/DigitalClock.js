ec8ba0440f15542d6dfbf264a42e97531a02a14a
Snail-Lu <snaillu@yeah.net>
2022-07-15 23:25:42 +0800

    数字时钟demo调整

--- src/components/DigitalClock/DigitalClock.js
+++ src/components/DigitalClock/DigitalClock.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
-import styles from './DigitalClock.module.scss'
+import React, { Component } from 'react';
+import styles from './DigitalClock.module.scss';
 
 class DigitalClock extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             clockArray: [
                 [0, 1, 2],
@@ -13,43 +13,47 @@ class DigitalClock extends Component {
                 [0, 1, 2, 3, 4, 5],
                 [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
             ]
-        }
+        };
     }
     componentDidMount() {
         // 刷新时间
         setInterval(() => {
-            this.getCurrentTime()
-        }, 200)
+            this.getCurrentTime();
+        }, 200);
     }
 
     // 获取当前时间并进行格式转换
     getCurrentTime = () => {
-        let { clockArray } = this.state
-        let date = new Date()
-        let hours = date.getHours()
+        let { clockArray } = this.state;
+        let date = new Date();
+        let hours = date.getHours();
         if (hours > 19) {
-            clockArray[1] = [0, 1, 2, 3]
+            clockArray[1] = [0, 1, 2, 3];
         }
-        hours = hours < 10 ? '0' + hours : hours + ''
-        let h = hours.split('')
+        hours = hours < 10 ? '0' + hours : hours + '';
+        let h = hours.split('');
 
-        let minutes = date.getMinutes()
-        minutes = minutes < 10 ? '0' + minutes : minutes + ''
-        let m = minutes.split('')
-        let seconds = date.getSeconds()
-        seconds = seconds < 10 ? '0' + seconds : seconds + ''
-        let s = seconds.split('')
+        let minutes = date.getMinutes();
+        minutes = minutes < 10 ? '0' + minutes : minutes + '';
+        let m = minutes.split('');
+        let seconds = date.getSeconds();
+        seconds = seconds < 10 ? '0' + seconds : seconds + '';
+        let s = seconds.split('');
 
-        let currentTime = h.concat(m, s)
-        this.setState({ h, m, s, currentTime, clockArray })
-    }
+        let currentTime = h.concat(m, s);
+        this.setState({ h, m, s, currentTime, clockArray });
+    };
     render() {
-        let { clockArray, currentTime = [] } = this.state
+        let { clockArray, currentTime = [] } = this.state;
         return (
             <div className={styles.clock_container}>
                 {clockArray.map((item, index) => {
                     return (
-                        <div className={styles.clock_item} key={index}>
+                        <div
+                            className={styles.clock_item}
+                            key={index}
+                            style={{ marginTop: (1 - currentTime[index]) * 30 + 'px' }}
+                        >
                             <div key={item[0].toString()}>
                                 {item.map((childItem) => (
                                     <div
@@ -61,11 +65,11 @@ class DigitalClock extends Component {
                                 ))}
                             </div>
                         </div>
-                    )
+                    );
                 })}
             </div>
-        )
+        );
     }
 }
 
-export default DigitalClock
+export default DigitalClock;
