c5f95d39350ef0f0cf6ba199467f28e78c5235d4
Snail-Lu <snaillu@yeah.net>
2022-09-06 18:11:13 +0800

    部署配置修改

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './redux/store'
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import request from './utils/request'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import request from './utils/request';
 
 // request挂载全局
-React.Component.prototype.req = request
-const root = ReactDOM.createRoot(document.getElementById('root'))
+React.Component.prototype.req = request;
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
+            <BrowserRouter basename="/portfolio-react">
                 <App />
             </BrowserRouter>
         </Provider>
     </React.StrictMode>
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
